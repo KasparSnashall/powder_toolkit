d232d266cfb80a7c7a58e8ca5699000853db1bfc
KasparSnashall <snashalk@tcd.ie>
2015-07-17 10:20:37 +0100

    Added the getters and setters, finished comparator

--- Powder_toolkit/src/powder_toolkit/Comparator.java
+++ Powder_toolkit/src/powder_toolkit/Comparator.java
@@ -23,41 +23,47 @@ public class Comparator {
 	
 	public static Dataset data1  = new DoubleDataset(new double[]{1,2,3,4,4,5,6,7,8,9,9,6,4,3,2,1,2});
 	public static Dataset data2 =  new DoubleDataset(new double[]{1,2,3,4,4,5,6,7,8,9,9,6,4,3,2,1,2});
-	public static boolean optimise = true; // optimise boolean
+	public static boolean optimise = false; // optimise boolean
 	public static int upper = 7; // upper range limit
 	public static int lower = 0; // lower range limit
+	public static boolean range_flag = false; // range flag
 	public static double tolerance = 5.0; // tolerance percentage
-	public static List<Double> weights = new ArrayList<Double>();
-	public static List<Integer> weight_lower = new ArrayList<Integer>();
-	public static List<Integer> weight_upper = new ArrayList<Integer>();
+	public static List<Double> weights = new ArrayList<Double>(); // the list of weights
+	public static List<Integer> weight_lower = new ArrayList<Integer>(); // weight lower limit
+	public static List<Integer> weight_upper = new ArrayList<Integer>(); // weight upper limit
 	
 	
 	
 	
-	public static Double binary() throws Exception{
-		
-		if (data1.getSize() == 0 | data2.getSize() == 0){ // check is not null
+	private static Double binary() throws Exception{
+		Dataset priv_data1 = data1; // private variables
+		Dataset priv_data2 = data2;
+		if (priv_data1.getSize() == 0 | priv_data2.getSize() == 0){ // check is not null
 			throw new Exception("Data size must not be zero");}
 		
-		if (data1.getSize() != data2.getSize()){
+		if (priv_data1.getSize() != priv_data2.getSize()){
 			// if data is not same size data1 is considered priority
-			int mysize = data1.getSize();
-			data2 = data2.getSliceView(new Slice(0,mysize));
+			int mysize = priv_data1.getSize();
+			priv_data2 = priv_data2.getSliceView(new Slice(0,mysize));
+		}
+		if(range_flag){
+			priv_data1 = slice_range(priv_data1);
+			priv_data2 = slice_range(priv_data2);
 		}
 		
 		if(optimise){ // optimiser also known as apply shift
-			data2 = optimiser();	
+			priv_data2 = optimiser(priv_data1,priv_data2);	
 		}
 		
-		final List<Integer> counts = new ArrayList<Integer>(); // counts list
-		for(int i = 0; i < data1.getSize(); i++){
-			double a = data1.getDouble(i); // element a
-			double b = data2.getDouble(i); // element b
-			Double tol = tolerance*data1.getDouble(i)/100.0; // percentage tolerance tol
+		final List<Double> counts = new ArrayList<Double>(); // counts list
+		for(int i = 0; i < priv_data1.getSize(); i++){
+			double a = priv_data1.getDouble(i); // element a
+			double b = priv_data2.getDouble(i); // element b
+			Double tol = tolerance*priv_data1.getDouble(i)/100.0; // percentage tolerance tol
 			if(a - tol <= b && b <= a+tol){ // is it in range?
-				counts.add(1);} // positive
+				counts.add(1.0);} // positive
 			else{
-				counts.add(0); // negative 
+				counts.add(0.0); // negative 
 			}
 		}
 		
@@ -76,10 +82,10 @@ public class Comparator {
 			}	
 	}
 	
-	private static double weighted_average(List<Integer> counts) {
+	private static double weighted_average(List<Double> percents) {
 		List<Double> kilos = new ArrayList<Double>();
 		// populates the kilos list with 1.0s
-		for(int i = 0; i < counts.size(); i++)
+		for(int i = 0; i < percents.size(); i++)
 		    kilos.add(1.0);
 		
 		for(int i=0; i < weights.size(); i++){
@@ -96,7 +102,7 @@ public class Comparator {
 		List<Double> grammes = new ArrayList<Double>();
 		for(int i = 0; i < kilos.size(); i++){
 			double weight = kilos.get(i); 
-			double value = counts.get(i);
+			double value = percents.get(i);
 			double milligramme = weight*value;
 			grammes.add(milligramme); // weights*counts list
 		}
@@ -112,11 +118,70 @@ public class Comparator {
 		return percent;
 	}
 
-	public static void fractional(){
-		//TODO
+	private static double fractional() throws Exception{
+		Dataset priv_data1 = data1;
+		Dataset priv_data2 = data2;
+		
+		if (priv_data1.getSize() == 0 | priv_data2.getSize() == 0){ // check is not null
+			throw new Exception("Data size must not be zero");}
+		
+		if (priv_data1.getSize() != priv_data2.getSize()){
+			// if data is not same size data1 is considered priority
+			int mysize = priv_data1.getSize();
+			priv_data2 = priv_data2.getSliceView(new Slice(0,mysize));
+		}
+		if(range_flag){
+			priv_data1 = slice_range(priv_data1);
+			priv_data2 = slice_range(priv_data2);
+		}
+		
+		List<Double> percents = new ArrayList<Double>();
+		for(int i = 0; i < priv_data1.getSize(); i ++){
+			double a = priv_data1.getDouble(i);
+			double b = priv_data2.getDouble(i);
+			if(a > 0.0 && b > 0.0){
+				if(a == b){
+					percents.add(100.0);
+				}
+				else if( a > b && a != b){
+					double p = 100.0*a/b;
+					percents.add(p);
+				}
+				else if (b > a && b!= a){
+					double p = 100.0*b/a;
+					percents.add(p);	
+				}
+			}
+			else if(a == 0.0 | b == 0.0){
+					if(a != b){
+						double p = 0.0;
+						percents.add(p);
+					}
+					else{
+						double p = 100.0;
+						percents.add(p);
+					}
+				}
+			else{
+				double p = 0.0;
+				percents.add(p);}
+			}
+	if(weights.size() != 0){
+		double percent = weighted_average(percents);
+		return percent;
+	}
+	else{
+		double sum = 0;
+		for(int i = 0; i < percents.size(); i++)
+		    sum += percents.get(i);
+		System.out.println(percents);
+		double percent = sum/percents.size();
+		return percent;
+	}
+		
 	}
 	
-	public static Dataset slice_range(Dataset dataset) throws Exception{
+	private static Dataset slice_range(Dataset dataset) throws Exception{
 		if (lower > upper){
 			 throw new Exception("Invalid range");
 		}
@@ -129,7 +194,8 @@ public class Comparator {
 		dataset = dataset.getSliceView(new Slice(lower,upper));
 		return dataset;}
 	
-	public static Dataset optimiser(){
+	
+	private static Dataset optimiser(Dataset priv_data1,Dataset priv_data2){
 		double step1 = -0.5;
 		double step2 = 0.5;
 		int stepnum = 300;
@@ -142,11 +208,11 @@ public class Comparator {
 			final List<Double> shifted = new ArrayList<Double>();
 			final List<Double> delx = new ArrayList<Double>();
 			Double sum = 0.0;
-			for(int i = 0; i < data2.getSize();i++){
+			for(int i = 0; i < priv_data2.getSize();i++){
 					
-					Double myshift = data2.getDouble(i)+mystep;
+					Double myshift = priv_data2.getDouble(i)+mystep;
 					shifted.add(myshift);
-					double deltax = data1.getDouble(i)-shifted.get(i);
+					double deltax = priv_data1.getDouble(i)-shifted.get(i);
 					delx.add(Math.pow(deltax, 2));
 					
 					for(int k = 0; k < delx.size(); k++){
@@ -157,22 +223,118 @@ public class Comparator {
 			average_shifts.add(sum);
 			
 			}
-		for(int i=0;i < data2.getSize();i++){
-			Double bestfit = data2.getDouble(i)+Collections.min(average_shifts);
-			data2.set(bestfit, i);
+		for(int i=0;i < priv_data2.getSize();i++){
+			Double bestfit = priv_data2.getDouble(i)+Collections.min(average_shifts);
+			priv_data2.set(bestfit, i);
 		}
-		System.out.println(Collections.min(average_shifts));
-		System.out.println(data2.toString(true));
+		return priv_data2;
+	}
+	
+
+	private static Double both() throws Exception {
+		// TODO Auto-generated method stub
+		Double percent1 = binary();
+		Double percent2 = fractional();
+		Double average = (percent1+percent2)/2;
+		return average;
+	}
+	
+	
+	public static Double Compare(String option) throws Exception{
+		if(option == "binary"){
+			// binary comparison
+			return binary();
+		}
+		else if(option == "fractional"){
+			// fractional comparison
+			return fractional();
+		}
+		else{
+			// defailt is the average
+			return both();
+		}
+	}
+
+	public static Dataset getData1() {
+		return data1;
+	}
+
+	public static void setData1(Dataset data1) {
+		Comparator.data1 = data1;
+	}
+
+	public static Dataset getData2() {
 		return data2;
-		// TODO
 	}
-	
-	public static void main(String[] args) throws Exception{
-		System.out.print("Comparator \n");
-		Dataset newdata = slice_range(data1);
-		binary();
+
+	public static void setData2(Dataset data2) {
+		Comparator.data2 = data2;
 	}
-	
+
+	public static boolean isOptimise() {
+		return optimise;
+	}
+
+	public static void setOptimise(boolean optimise) {
+		Comparator.optimise = optimise;
+	}
+
+	public static int getUpper() {
+		return upper;
+	}
+
+	public static void setUpper(int upper) {
+		Comparator.upper = upper;
+	}
+
+	public static int getLower() {
+		return lower;
+	}
+
+	public static void setLower(int lower) {
+		Comparator.lower = lower;
+	}
+
+	public static boolean isRange_flag() {
+		return range_flag;
+	}
+
+	public static void setRange_flag(boolean range_flag) {
+		Comparator.range_flag = range_flag;
+	}
+
+	public static double getTolerance() {
+		return tolerance;
+	}
+
+	public static void setTolerance(double tolerance) {
+		Comparator.tolerance = tolerance;
+	}
+
+	public static List<Double> getWeights() {
+		return weights;
+	}
+
+	public static void setWeights(List<Double> weights) {
+		Comparator.weights = weights;
+	}
+
+	public static List<Integer> getWeight_lower() {
+		return weight_lower;
+	}
+
+	public static void setWeight_lower(List<Integer> weight_lower) {
+		Comparator.weight_lower = weight_lower;
+	}
+
+	public static List<Integer> getWeight_upper() {
+		return weight_upper;
+	}
+
+	public static void setWeight_upper(List<Integer> weight_upper) {
+		Comparator.weight_upper = weight_upper;
+	}
+
 	
 
 }
