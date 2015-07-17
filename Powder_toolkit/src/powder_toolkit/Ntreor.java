e7b1a71d6f7695cc088b28bd3bc3f274e1fbe8df
KasparSnashall <snashalk@tcd.ie>
2015-07-17 11:53:17 +0100

    First exe file class

---
+++ Powder_toolkit/src/powder_toolkit/Ntreor.java
@@ -0,0 +1,24 @@
+package powder_toolkit;
+
+public class Ntreor extends Execution {
+	
+	@Override
+	public String Run() {
+		filepath = "";
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write_input() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read_output() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
