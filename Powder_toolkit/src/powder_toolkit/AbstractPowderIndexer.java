0f14aba8aeb939177f41c3ad5d65ce70566c8ebe
KasparSnashall <snashalk@tcd.ie>
2015-07-17 16:18:55 +0100

    Added Abstract

--- Powder_toolkit/src/powder_toolkit/AbstractPowderIndexer.java
+++ Powder_toolkit/src/powder_toolkit/AbstractPowderIndexer.java
@@ -1,13 +1,12 @@
 package powder_toolkit;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
 
-public abstract class Execution {
+public abstract class AbstractPowderIndexer implements IPowderIndexer {
 	
 	
 	public static Map<String,String> standard_dict = new HashMap<String, String>(); // have a blank dictionary
@@ -15,7 +14,33 @@ public abstract class Execution {
 	public static String filepath = "";
 	public static Dataset data = new DoubleDataset();
 	public static final String title = "";
-	//public abstract void write_input();
-	public abstract void read_output();
+	public Map<String, String> getStandard_dict() {
+		return standard_dict;
+	}
+	public void setStandard_dict(Map<String, String> standard_dict) {
+		AbstractPowderIndexer.standard_dict = standard_dict;
+	}
+	public Map<String, String> getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(Map<String, String> keywords) {
+		AbstractPowderIndexer.keywords = keywords;
+	}
+	public String getFilepath() {
+		return filepath;
+	}
+	public void setFilepath(String filepath) {
+		AbstractPowderIndexer.filepath = filepath;
+	}
+	public Dataset getData() {
+		return data;
+	}
+	public void setData(Dataset data) {
+		AbstractPowderIndexer.data = data;
+	}
+	public String getTitle() {
+		return title;
+	}
+	
 
 }
