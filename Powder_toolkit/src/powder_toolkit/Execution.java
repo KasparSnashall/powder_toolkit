a537e7c581c56a63831ce148bffa5c76473245a1
KasparSnashall <snashalk@tcd.ie>
2015-07-17 11:53:06 +0100

    Created abstract class for exe files

---
+++ Powder_toolkit/src/powder_toolkit/Execution.java
@@ -0,0 +1,21 @@
+package powder_toolkit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+
+public abstract class Execution {
+	
+	
+	public static Map<String,String> standard_dict = new HashMap<String, String>(); // have a blank dictionary
+	public static Map<String,String> keywords = new HashMap<String,String>(); 
+	public static String filepath = "";
+	public static Dataset data = new DoubleDataset();
+	public static final String title = "";
+	public abstract String Run();
+	public abstract void write_input();
+	public abstract void read_output();
+
+}
