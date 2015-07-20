490b0310aed54a35190f49116ea4c7d077b088fe
KasparSnashall <snashalk@tcd.ie>
2015-07-20 12:52:16 +0100

    add read and write

--- Powder_toolkit/src/powder_toolkit/Ntreor.java
+++ Powder_toolkit/src/powder_toolkit/Ntreor.java
@@ -4,27 +4,33 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.dawnsci.analysis.api.dataset.Slice;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.osgi.framework.adaptor.FilePath;
 
 public class Ntreor extends AbstractPowderIndexer {
 	
 	//keys
-	public String[] keys = {"KH","KK","KL","KS","THH","THL","THK","THS","OH1","OK1","OL1","OS1","OH2","OK2","OL2","OS2","OH3","OK3","OL3","OS3","MH1","MK1","ML1",
+	public static String[] keys = {"KH","KK","KL","KS","THH","THL","THK","THS","OH1","OK1","OL1","OS1","OH2","OK2","OL2","OS2","OH3","OK3","OL3","OS3","MH1","MK1","ML1",
             "MS1","MH2","MK2","ML2","MS2","MH3","MK3","ML3","MS3","MH4","MK4","ML4","MS4",
             "MONOSET","MONOGRAM","MONO","SHORT","USE","IQ","LIST","SELECT","MERIT",
             "NIX","IDIV","WAVE","VOL","CEM","D1","SSQTL","D2",
             "CHOICE","DENS","EDENS","MOLW","TRIC"};
 	//values
-	public String[] standard_values = {"4","4","4","6","4","4","4","4","2","2","2","3","2","2","2","4","2","2",
+	public static String[] standard_values = {"4","4","4","6","4","4","4","4","2","2","2","3","2","2","2","4","2","2",
 			"2","4","2","2","2","2","2","2","2","3","2","2","2","2","2","2","2","2","0","1","0","1",
 			"19","16","0","0","10","1","1","1.5405981","2000","25","0.002","0.05","0.0004",
             "0","0","0","0","0"};
@@ -32,26 +38,64 @@ public class Ntreor extends AbstractPowderIndexer {
 	public void write_input() {
 		// create a dataset
 		Dataset data1 = data.getSliceView(new Slice(0,20));
-		if(title.contains(".")){
-			String mytitle = title.split(".")[0];
+		String mytitle = title;
+		if(mytitle != null && mytitle.contains(".")){
+			String[] mytitlelist = mytitle.split("\\.");
+			mytitle = mytitlelist[0];
 		}
-		
 		try {
-			File file = new File("myfile.txt");
-			FileWriter write = new FileWriter(file);
+			PrintWriter writer = new PrintWriter("/scratch/runfiles/mytest.dat", "UTF-8");
+			writer.println(mytitle);
+			// write in the data
+			for(int i =0; i< data1.getSize(); i++){
+				double d = data1.getDouble(i);
+				String d1 = String.valueOf(d);
+				writer.println(d1);
+			}
+			// write in the key words
+			for (Entry<String, String> entry : keywords.entrySet()) {
+			    String key = entry.getKey();
+			    Object value = entry.getValue();
+			    writer.println(key +" = "+value+",");
+			}
+			// finish file
+			writer.println("END*");
+			writer.println("0.00");
+			writer.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	@Override
-	public void read_output() {
-		// TODO Auto-generated method stub
+	public List<String> read_output() {
+		List<String> output = new ArrayList<String>(); // my output array
+		try{
+		BufferedReader br = new BufferedReader(new FileReader("/scratch/runfiles/treor90_output.imp"));
+		String line; // line variable
+		while ((line = br.readLine()) != null) {
+		   // process the line.
+		   if (line.contains("TOTAL NUMBER OF LINES ="))
+		   {
+		       try {
+		    	for(int i = 0; i<3;i++){
+				line = br.readLine(); // grab the 3 lines after
+				output.add(line); // append the output
+				}
+		    	
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		   }
+		}
+		br.close();
+		}
+		catch(Exception e){
+			System.out.println(e);}
 		
+	return output; // cleaned output
 	}
 
-	public static List<String> Run(){
+	public List<String> Run(){
 		List<String> output = new ArrayList<String>();
 		try{
 			Runtime rt = Runtime.getRuntime(); // exe
@@ -62,10 +106,10 @@ public class Ntreor extends AbstractPowderIndexer {
 		    
 		    String line = null; // output
 		    bw.write("N\n"); // general info?
-		    bw.write("../../../runfiles/treor90.dat"+"\n"); // dat location
-            bw.write("../../../runfiles/a.imp"+"\n"); // new imp location
-            bw.write("../../../runfiles/b.con"+"\n"); // condensed location
-            bw.write("../../../runfiles/c.short"+"\n"); // short location
+		    bw.write(filepath+title+".dat"+"\n"); // dat location
+            bw.write(filepath+title+".imp"+"\n"); // new imp location
+            bw.write(filepath+title+".con"+"\n"); // condensed location
+            bw.write(filepath+title+".short"+"\n"); // short location
             bw.write("0\n"); // theta shift
             bw.write("N\n"); // stop after 1 iteration?
             bw.flush(); // flush the output
@@ -84,14 +128,15 @@ public class Ntreor extends AbstractPowderIndexer {
 			}
 		return output;
 	}
-	public static void main(String[] args){
-		//List<String> run = Run();
-		//System.out.println(run);
+
+	// overide the previous
+	public String[] getKeys(){
+		return keys;
 		
 	}
-	
-	
-	////////////getters and setters ///////
-	
+	// overide
+	public String[] getStandard_values(){
+		return standard_values;
+	}
 
 }
