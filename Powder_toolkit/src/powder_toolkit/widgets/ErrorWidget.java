b529e1e6d9799ae1d35fac1a16ac532cb0a15f29
KasparSnashall <snashalk@tcd.ie>
2015-07-31 16:53:15 +0100

    Added in error widget to deal with errors and send them to the user via a new shell

---
+++ Powder_toolkit/src/powder_toolkit/widgets/ErrorWidget.java
@@ -0,0 +1,42 @@
+package powder_toolkit.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Shell;
+
+public class ErrorWidget  {
+	
+	public ErrorWidget(Exception e){
+		Shell myshell = new Shell();
+		myshell.setText("Error");
+		Composite composite = new Composite(myshell,SWT.FILL);
+		GridLayout layout = new GridLayout(1,false);
+	    layout.marginWidth = 25;
+	    layout.marginHeight = 25;
+	    composite.setLayout(layout);
+		
+		Label myerror = new Label(composite,SWT.NONE);
+		myerror.setText(e.getMessage());
+		GridData grid = new GridData(SWT.FILL,SWT.FILL,true,true,1,1);
+		myerror.setLayoutData(grid);
+		composite.pack();
+		myshell.setSize(200, 100);
+		Monitor primary = Display.getCurrent().getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = myshell.getBounds();
+	    
+	    int x = bounds.x + (bounds.width - rect.width) / 2;
+	    int y = bounds.y + (bounds.height - rect.height) / 2;
+	    
+	    myshell.setLocation(x, y);
+		myshell.open();
+		
+	}
+
+}
