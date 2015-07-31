c99b3f303aaa80341029ecec3256c590ad85c761
KasparSnashall <snashalk@tcd.ie>
2015-07-31 15:43:15 +0100

    Added a sh file to run search csd

---
+++ Powder_toolkit/python_code/csd.sh
@@ -0,0 +1,4 @@
+module load python/ana 
+module load ccdc/api-quiet 
+source activate ccdc 
+python /scratch/clean_workpsace/powder_toolkit/Powder_toolkit/python_code/CSD_cell_search.py 10.0 10.0 10.0 90.0 90.0 90.0 primitive 5
