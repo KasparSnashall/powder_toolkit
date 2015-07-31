f2a82124ea85750b1f6d16822b524c97def4d43b
KasparSnashall <snashalk@tcd.ie>
2015-07-31 15:43:30 +0100

    CSD now runs from a command line instead of jython

--- Powder_toolkit/python_code/CSD_cell_search.py
+++ Powder_toolkit/python_code/CSD_cell_search.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append("/home/sfz19839/Desktop/api")
 
 from ccdc.search import ReducedCellSearch
-from ccdc.collections import namedtuple
-
+#from ccdc.collections import namedtuple
+from collections import namedtuple
 
 class CSD_cell_search:
     """
@@ -38,40 +38,48 @@ class CSD_cell_search:
     
     
     def __init__(self):
-        1
-    def Printme(self):
-        return "here"
-    
-    def get_angles(self):
-        return self.angles
+        pass
     def set_angles(self,angles):
-        print angles
         self.angles = angles
-    def get_cell_lengths(self):
-        
-        return self.cell_lengths
     def set_cell_lengths(self,lengths):
-        print lengths
         self.cell_lengths = lengths
-    def get_lattice_centring(self):
-        return self.lattice_centring
     def set_lattice_centring(self,centring):
         self.lattice_centring = centring
+    def set_number_hits(self,hits):
+        self.numhits = hits  
+    
     
     def search(self):
         # searches the csd data base for a crystal with known parameters returns best result
         cellA = namedtuple('CellAngles', ['alpha', 'beta', 'gamma']) # create a named tuple for ccdc module
         angles = list(self.angles) # make a list of angles
-        more_angles = cellA(angles[0],angles[1],angles[2]) # append angles to the tuple
-        
+        more_angles = cellA(float(angles[0]),float(angles[1]),float(angles[2])) # append angles to the tuple
         cellL = namedtuple('CellLengths',['a','b','c'])
         cell_lengths = self.cell_lengths
-        more_lengths = cellL(cell_lengths[0],cell_lengths[1],cell_lengths[2])
+        more_lengths = cellL(float(cell_lengths[0]),float(cell_lengths[1]),float(cell_lengths[2]))
         
         query = ReducedCellSearch.Query(lengths = more_lengths,angles = more_angles ,lattice_centring = 'primitive' )
         searcher = ReducedCellSearch(query)
         
         hits = searcher.search() # search for all hits
-        h = hits[0] # top hit
-        print h.identifier, h.crystal.cell_lengths, h.crystal.cell_angles, h.crystal.lattice_centring # print the best result 
+        i = 0
+        while i < float(self.numhits):
+            h = hits[i]
+            print h.identifier, h.crystal.cell_lengths, h.crystal.cell_angles, h.crystal.lattice_centring # print the best result 
+            i += 1
+        
 
+if __name__ == '__main__':
+    CSD = CSD_cell_search()
+    x1 = sys.argv[1]
+    x2 = sys.argv[2]
+    x3 = sys.argv[3]
+    CSD.set_cell_lengths([x1,x2,x3])
+    Y1 = sys.argv[4]
+    Y2 = sys.argv[5]
+    Y3 = sys.argv[6]
+    myhits = sys.argv[8]
+    CSD.set_angles([Y1,Y2,Y3])
+    CSD.set_lattice_centring(sys.argv[7])
+    CSD.set_number_hits(myhits)
+    CSD.search()
