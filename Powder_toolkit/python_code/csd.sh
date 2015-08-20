daf05c04caf2c6399ad0b96245912dfe1cfe58bf
KasparSnashall <snashalk@tcd.ie>
2015-08-20 13:58:34 +0100

    Generated java docs

--- Powder_toolkit/python_code/csd.sh
+++ Powder_toolkit/python_code/csd.sh
@@ -1,2 +1,2 @@
 source /dls_sw/apps/ccdc/scripts/ccdc-api.sh
-python /scratch/clean_workpsace/powder_toolkit/Powder_toolkit/python_code/CSD_cell_search.py 10.0 10.0 10.0 90.0 90.0 90.0 primitive 1
+python /scratch/clean_workpsace/powder_toolkit/Powder_toolkit/python_code/CSD_cell_search.py 10.0 10.0 10.0 90.0 90.0 90.0 primitive 10
